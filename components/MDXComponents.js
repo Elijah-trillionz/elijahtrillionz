aa3d185652ad81f359dc084f60b801788070f303
Elijah Trillionz <elijahtrillionz@gmail.com>
2022-02-16 09:58:05 +0100

    fixing bug

--- components/MDXComponents.js
+++ components/MDXComponents.js
@@ -6,19 +6,18 @@ import {
   ArticleHeader,
   ArticleQuote,
   SyntaxHighlighter,
-} from "./styles/Markdown.styled";
-import prism from "prismjs";
-import "prismjs/components/prism-python";
-import "prismjs/components/prism-typescript";
-import "prismjs/components/prism-javascript";
-import { useEffect } from "react";
+} from './styles/Markdown.styled';
+import prism from 'prismjs';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-javascript';
 
 const CodeHighlighter = ({
   children: {
     props: { className, children },
   },
 }) => {
-  const language = className.split("-").at(-1);
+  const language = className.split('-').at(-1);
   const codeElement = () => {
     return {
       __html: prism.highlight(children, prism.languages[language], language),
@@ -27,7 +26,7 @@ const CodeHighlighter = ({
 
   return (
     <SyntaxHighlighter>
-      <code dangerouslySetInnerHTML={codeElement()} className="some-lang" />
+      <code dangerouslySetInnerHTML={codeElement()} className='some-lang' />
     </SyntaxHighlighter>
   );
 };
@@ -37,14 +36,14 @@ const MDXComponents = {
   a: (props) => <ArticleLink {...props} />,
   h1: (props) => <ArticleTitle {...props} />,
   ol: (props) => <ArticleList {...props} />,
-  ul: (props) => <ArticleList {...props} as={"ul"} />,
+  ul: (props) => <ArticleList {...props} as={'ul'} />,
   blockquote: (props) => <ArticleQuote {...props} />,
-  h2: (props) => <ArticleHeader {...props} style={{ fontSize: "1.3rem" }} />,
-  h3: (props) => <ArticleHeader {...props} as={"h3"} />,
-  h4: (props) => <ArticleHeader {...props} as={"h4"} />,
-  h5: (props) => <ArticleHeader {...props} as={"h5"} />,
-  h6: (props) => <ArticleHeader {...props} as={"h6"} />,
-  pre: (props) => <CodeHighlighter {...props} />,
+  h2: (props) => <ArticleHeader {...props} style={{ fontSize: '1.3rem' }} />,
+  h3: (props) => <ArticleHeader {...props} as={'h3'} />,
+  h4: (props) => <ArticleHeader {...props} as={'h4'} />,
+  h5: (props) => <ArticleHeader {...props} as={'h5'} />,
+  h6: (props) => <ArticleHeader {...props} as={'h6'} />,
+  pre: (props) => <CodeHighlighter {...props} className='some' />,
 };
 
 export default MDXComponents;
