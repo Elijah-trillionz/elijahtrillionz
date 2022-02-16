13c0db32a3e401cb06a1a260fb3068ca71ad6bbe
Elijah Trillionz <elijahtrillionz@gmail.com>
2022-02-16 02:15:52 +0100

    changed favicon and image

--- components/MeetMe.js
+++ components/MeetMe.js
@@ -1,22 +1,22 @@
-import Image from "next/image";
-import { StyledAbout } from "./styles/Home.styled";
+import Image from 'next/image';
+import { StyledAbout } from './styles/Home.styled';
 
 const MeetMe = () => {
   return (
     <StyledAbout>
       <Image
-        src="/elijah_trillionz.jpg"
-        alt="john doe avatar"
+        src='/user/profile_image/475315/8ca55abe-8c92-48cb-bc29-4de6e64f4921.jpg'
+        alt='john doe avatar'
         width={130}
         height={130}
       />
       <p>
-        Hello there, am{" "}
+        Hello there, am{' '}
         <strong>
           <a
-            href="https://twitter.com/elijahtrillionz"
-            target={"_blank"}
-            rel={"noreferrer"}
+            href='https://twitter.com/elijahtrillionz'
+            target={'_blank'}
+            rel={'noreferrer'}
           >
             Elijah
           </a>
--- next.config.mjs
+++ next.config.mjs
@@ -20,4 +20,8 @@ export default {
   },
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+  images: {
+    loader: 'imgix',
+    path: 'https://res.cloudinary.com/practicaldev/image/fetch/s--DBl8InY4--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/',
+  },
 };
